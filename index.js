648c3aaed60fa4a0639fc19f9e1de38884a508c6
Aydin Utting <aydinu@Aydins-MacBook-Pro-2020.local>
2021-10-12 17:39:50 +0100

    Added navbar and timer

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 document.getElementById("TITLE").innerHTML = "Aydin's Page"
+const start = Date.now();
+function stopwatch() {
+    setTimeout(stopwatch,10);
+    const end = Date.now();
+    const ellapsed = end-start;
+    const hours = Math.floor(ellapsed/3.6e+6)
+    const minutes = Math.floor((ellapsed%3.6e+6)/60000)
+    const seconds = Math.floor((ellapsed%60000)/1000)
+    const milliseconds = ellapsed%1000
+    document.getElementById("timer-display").innerHTML = _.padStart(hours,2,'0')+
+    ':'+_.padStart(minutes,2,'0')+
+    ':'+_.padStart(seconds,2,'0')+
+    '.'+_.padEnd(milliseconds,3,'0');
+
+}
