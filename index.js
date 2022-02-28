4f7c334b9508306cf526b5f5fc32d73a604de540
aydin-utting <aydinutting@gmail.com>
2022-02-28 11:00:30 +0000

    updated timer to check for focus

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 document.getElementById("TITLE").innerHTML = "Aydin's Page"
-const start = Date.now();
+let ellapsed = 0;
+
 function stopwatch() {
-    setTimeout(stopwatch,10);
-    const end = Date.now();
-    const ellapsed = end-start;
+    setInterval(updateTime, 10);
+}
+
+function updateTime() {
+    if (document.hasFocus()) {
+    	ellapsed = ellapsed+10;
+    }
     const hours = Math.floor(ellapsed/3.6e+6)
     const minutes = Math.floor((ellapsed%3.6e+6)/60000)
     const seconds = Math.floor((ellapsed%60000)/1000)
@@ -14,3 +19,4 @@ function stopwatch() {
     '.'+_.padEnd(milliseconds,3,'0');
 
 }
+
